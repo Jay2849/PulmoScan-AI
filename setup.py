171215f170fd6364c8247a13dbcb4e2447169746
Jay Negi <jayn75009@gmail.com>
2026-08-14 14:34:07 +0530

    feat: Add setup.py package configuration for PulmoScan-AI

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="PulmoScan-AI",
+    version="0.0.1",
+    author="Jay Negi",
+    author_email="jayn75009@gmail.com",
+    url=f"https://github.com/Jay2849/PulmoScan-AI",
+    packages=find_packages(),   
+)
